cff3f0a9a064034d5b20a17665e217978242234e
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-01 16:43:23 +0800

    Added download class for downloading methods

---
+++ includes/download.php
@@ -0,0 +1,28 @@
+<?php
+
+class Download
+{
+    public function downloadTrack()
+    {
+        if(isset($_GET['path'])) {
+        $filename = $_GET['path'];
+        if(file_exists($filename)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header("Cache-Control: no-cache, must-revalidate");
+            header("Expires: 0");
+            header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+            header('Content-Length: ' . filesize($filename));
+            header('Pragma: public');
+        
+            flush();
+            
+            readfile($filename);
+            die();
+            } else {
+            echo "File does not exist.";
+            }
+        }
+    }
+}
+?>
