9b2e13fc7b64373fd85ebe8c42e41b268097c54c
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-08 07:31:50 +0800

    refactor: created mail.php and mail class with methods from functions.php

---
+++ includes/mail.php
@@ -0,0 +1,48 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+Class Mail
+{
+    public function send($email, $body)
+    {
+        if(isset($_POST['submit'])) {
+            $mail = new PHPMailer(true);
+
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = "";
+            $mail->Password = '';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            $mail->setFrom('kyle.estocapio031@gmail.com');
+            $mail->addAddress($email);
+            $mail->isHTML(true);
+
+            $mail->Subject = "Order from Music locker";
+            $mail->Body = $body;
+
+            $mail->send();
+
+            echo
+            "
+            <script>
+            alert('Purchase Successfully, Order Details sent via Email');
+            document.location.href='shop.php';
+            </script>
+            ";
+        }
+    }
+
+    public function setBody()
+    {
+        
+    }
+}
