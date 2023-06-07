5bc4e1b882e8ae784b5daa4d50440456d1d88040
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-08 07:32:09 +0800

    refactor: created message.php and message class with methods from functions.php

---
+++ includes/message.php
@@ -0,0 +1,21 @@
+<?php
+
+Class Message
+{
+    public function set($message)
+    {
+        if (!empty($message)) {
+            $_SESSION['message'] = $message;
+        } else {
+            $message = "";
+        }
+    }
+
+    public function show()
+    {
+        if (isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+        }
+    }
+}
