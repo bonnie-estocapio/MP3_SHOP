5c2a1b2babb410abe85a658a3730fbf15d398525
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-08 07:34:08 +0800

    refactor: created navbar.php containing page navigation bar html and php code

---
+++ templates/navbar.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'includes/autoload.php';
+
+$autoload = new Autoload;
+$functions = new Functions;
+$guest = $functions->state();
+?>
+
+</head>
+<body>
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Music Locker</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="Shop.php">Shop</a>
+                    </li>
+                    <?php if ($guest === true) {?>
+                    <li>                        
+                        <a href="login.php">Login</a>
+                    </li>
+                    <?php } ?>
+                    <?php if ($guest === false) {?>
+                    <li>                        
+                        <a href="logout.php">Logout</a>
+                    </li>
+                    <?php } ?>
+                     <li>
+                        <a href="checkout.php">Checkout</a>
+                    </li>
+                    <?php if ($guest === false) {?>
+                    <li>
+                        <a href="profile.php"><?=$_SESSION['user'];?></a>
+                    </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
