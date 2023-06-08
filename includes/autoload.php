b9483ec4891aa589dd4a38dd24d98395dd6b44e3
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-08 08:00:16 +0800

    refactor: modified to function call instead of Class

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,12 +1,6 @@
 <?php
 
-Class Autoload
-{
-    public function __construct()
-    {
-        spl_autoload_register(function ($path) {
-            $path = __DIR__ . '/' . lcfirst(str_replace('\\','/', $path)) . '.php';
-            require $path;
-        });
-    }
-}
+spl_autoload_register(function ($path) {
+    $path = __DIR__ . '/' . lcfirst(str_replace('\\','/', $path)) . '.php';
+    require $path;
+});
