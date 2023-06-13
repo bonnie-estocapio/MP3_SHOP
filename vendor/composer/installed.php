2eda9d35336c7e01392a63f3a68039f3649a8098
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-13 10:48:55 +0800

    feat: installed autoloader using composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '98d2564ca3eb4d72b6b02c9bec7be1674ceb28ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '98d2564ca3eb4d72b6b02c9bec7be1674ceb28ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
