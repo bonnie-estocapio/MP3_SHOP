210ef00a6cedea00237430e934c190269ad9a794
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-14 02:01:03 +0800

    refactor: Added spaces for PSR-12

--- src/app/Order/order.php
+++ src/app/Order/order.php
@@ -24,13 +24,13 @@ Class Order
     public function readLog($orderID, $username)
     {
         $dbase = new Database;
-        if ($orderID === NULL){
+        if ($orderID === NULL) {
             $orderQuery = $dbase->query("SELECT id, data, date FROM history WHERE username = '{$username}'");
             while($row = $orderQuery->fetch(\PDO::FETCH_ASSOC)){
                 $this->show($row);
             }
         } else {
-            $orderQuery = $$dbase->query("SELECT id, data, date FROM history WHERE username = '{$username}' && id = '{$orderID}'");
+            $orderQuery = $dbase->query("SELECT id, data, date FROM history WHERE username = '{$username}' && id = '{$orderID}'");
             while($row = $orderQuery->fetch(\PDO::FETCH_ASSOC)){
                 $this->show($row);
             }
@@ -40,13 +40,11 @@ Class Order
     public function show($orderData)
     {
         $track = new Track;
-        $functions = new Functions;
-
         $orderArray = [];
-
         parse_str($orderData['data'], $orderArray);
+
         foreach ($orderArray as $data => $value) {
-            if ($value === 1 && substr($data, 0, 8)== "product_"){
+            if ($value === 1 && substr($data, 0, 8) == "product_"){
                 $id = substr($data, 8, strlen($data) - 8);
                 $trackQuery = $track->getQuery($id);
                 while($row = $trackQuery->fetch(\PDO::FETCH_ASSOC)) {
