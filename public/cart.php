e8389e71ed595473a2fda270d0802f83ee7dc9c1
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-13 15:31:17 +0800

    refactor: added namespace and require autoloading

--- public/cart.php
+++ public/cart.php
@@ -1,6 +1,12 @@
 <?php
 
-require_once 'includes/autoload.php';
+namespace App\Public;
+
+require '../vendor/autoload.php';
+
+use App\Operation\Functions;
+use App\Order\Cart;
+
 session_start();
 
 $functions = new Functions;
