e459620667558638d56609a9ca457f1a766ac409
bonnie-estocapio <134898783+bonnie-estocapio@users.noreply.github.com>
2023-06-22 08:38:12 +0800

    refactor: transferred create account methods to Create class

--- src/app/User/user.php
+++ src/app/User/user.php
@@ -20,45 +20,32 @@ class User
             $username = $_POST['username'];
             $password = $_POST['password'];
 
-            $query = $database->conn->prepare("SELECT id, username FROM users WHERE username = :username AND password = :password");
+            $query = $database->conn->prepare("SELECT id, username, password, salt FROM users WHERE username = :username");
             $query->bindParam(':username', $username);
-            $query->bindParam(':password', $password);
             $query->execute();
 
             $data = $query->fetch(\PDO::FETCH_ASSOC);
 
             if (!$data) {
-                $message->set("Invalid Login Details");
+                $message->set("Invalid Username");
             } else {
-                $_SESSION['loggedin'] = true;
-                $_SESSION['user'] = $username;
-                $userdata->read();
-                $session->write(session_id(), $_SESSION['user']);
-                $navigation->goTo("index.php");
-                $session->setExpiry();
-            }
+                $correct = $this->checkPassword($password, $data['password'], $data['salt']);
+
+                if (!$correct) {
+                    $message->set("Invalid Password");
+                } else {
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['user'] = $data['username'];
+                    $userdata->read();
+                    $session->write(session_id(), $_SESSION['user']);
+                    $navigation->goTo("index.php");
+                    $session->setExpiry();
+                }
+            } 
         }
     }
 
-    public function create($username, $password, $fullname, $address, $email)
-    {
-        $database = new Database;
-        $message = new Message;
-
-        $query = $database->conn->prepare("INSERT INTO users (username, password, fullname, address, email) VALUES (:username, :password, :fullname, :address, :email)");
-        $query->bindParam(':username', $username);
-        $query->bindParam(':password', $password);
-        $query->bindParam(':fullname', $fullname);
-        $query->bindParam(':address', $address);
-        $query->bindParam(':email', $email);
-        $query->execute();
-        
-        if ($query) {
-            $message->set("Account Created, Please login");
-        }
-    }
-
-    public function logout()
+    public function logout(): void
     {
         $database = new Database;
         $userdata = new UserData;
@@ -84,78 +71,15 @@ class User
         header("Location: index.php");
     }
 
-    public function signUp()
+    public function checkPassword($password, $storedPassword, $salt): bool
     {
-        $navigation = new Navigation;
+        $create = new Create;
+        $hashedPassword = $create->hashPassword($password, $salt);
 
-        if (isset($_POST['signup'])) {
-            $navigation->goTo("register.php");
-        }
-    }
-
-    public function validate($username, $password, $fullname, $address, $email): void
-    {
-        $message = new Message;
-
-        $valid = true;
-        $errorMessage = '';
-
-        if (!preg_match('/^[a-zA-Z0-9_]{3,20}$/', $username)) {
-            $errorMessage = "Invalid Username </br>";
-            $valid = false;
-        }
-
-        if (!preg_match('/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/', $password)) {
-            $errorMessage .= "Invalid Password.</br>";
-            $valid = false;
-        }
-
-        if (!preg_match('/^[a-zA-Z ]+$/', $fullname)) {
-            $errorMessage .= "Invalid Full Name. </br>";
-            $valid = false;
-        }
-
-        if (!preg_match( '/^\d+ [a-zA-Z ]+, [a-zA-Z ]+, [a-zA-Z ]+, [a-zA-Z ]+$/', $address)) {
-            $errorMessage .= "Invalid Address. </br>";
-            $valid = false;
-        }
-
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errorMessage .= "Invalid Email. </br>";
-            $valid = false;
-        }
-        
-        if ($valid) {
-            $this->create($username, $password, $fullname, $address, $email);
+        if ($hashedPassword === $storedPassword) {
+            return true;
         } else {
-            $message->set($errorMessage . "Please try again.");
-        }
-    }
-
-    public function register()
-    {
-        $message = new Message;
-        
-        if (isset($_POST['submit'])) {
-            $incomplete = false;
-
-            foreach ($_POST as $data) {
-                if ($data === "") {
-                    $incomplete = true;
-                }
-            }
-
-            if ($incomplete === false) {
-                $this->validate(
-                    $_POST['username'],
-                    $_POST['password'],
-                    $_POST['fullname'],
-                    $_POST['address'],
-                    $_POST['email']
-                );
-            } else {
-                $message->set("Some fields are not filled. Try again.");
-            }
+            return false;
         }
     }
 }
